91ffe6e5e46be4e25bcbc3becb7fcd7cf5f2f9e7
ender2021 <meadmiracle@gmail.com>
2019-03-16 23:13:46 -0700

    Refactored Rest Request connection passing

--- modules/PJ-Project.psm1
+++ modules/PJ-Project.psm1
@@ -1,14 +1,24 @@
 #https://developer.atlassian.com/cloud/jira/platform/rest/v2/#api-rest-api-2-project-projectIdOrKey-versions-get
-function Invoke-JiraGetProjectVersions($JiraConnection=$Global:PJ_JiraSession,$ProjectKey) {
+function Invoke-JiraGetProjectVersions($JiraConnection,$ProjectKey) {
     $functionPath = "/rest/api/2/project/$ProjectKey/versions"
-    Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionAddress $functionPath -HttpMethod GET
+
+    if($JiraConnection) {
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionAddress $functionPath -HttpMethod GET
+    } else {
+        Invoke-JiraRestRequest -FunctionAddress $functionPath -HttpMethod GET
+    }
 }
 
 #https://developer.atlassian.com/cloud/jira/platform/rest/v2/#api-rest-api-2-project-projectIdOrKey-get
-function Invoke-JiraGetProject($JiraConnection=$Global:PJ_JiraSession,$ProjectId,$ProjectKey) {
+function Invoke-JiraGetProject($JiraConnection,$ProjectId,$ProjectKey) {
     $param = if ($ProjectId) { $ProjectId } else { $ProjectKey }
     $functionPath = "/rest/api/2/project/$param"
-    Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionAddress $functionPath -HttpMethod GET
+    if($JiraConnection) {
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionAddress $functionPath -HttpMethod GET
+    } else {
+        Invoke-JiraRestRequest -FunctionAddress $functionPath -HttpMethod GET
+    }
+    
 }
 
 Export-ModuleMember -Function * -Variable *
