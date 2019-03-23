703c77ca6b3db06cec6869ff5aad1f6f49fd40c7
ender2021 <meadmiracle@gmail.com>
2019-03-23 13:53:14 -0700

    Added Invoke-JiraGetComponentIssuesCount

--- modules/api-v2/PJ-Component.psm1
+++ modules/api-v2/PJ-Component.psm1
@@ -149,4 +149,25 @@ function Invoke-JiraDeleteComponent {
     }
 }
 
+#https://developer.atlassian.com/cloud/jira/platform/rest/v2/#api-rest-api-2-component-id-relatedIssueCounts-get
+function Invoke-JiraGetComponentIssuesCount {
+    [CmdletBinding()]
+    param (
+        # The ID of the Component to retrieve
+        [Parameter(Mandatory,Position=0)]
+        [int32]
+        $ComponentId,
+
+        # The JiraConnection object to use for the request
+        [Parameter(Position=1)]
+        [hashtable]
+        $JiraConnection
+    )
+    process {
+        $functionPath = "/rest/api/2/component/$ComponentId/relatedIssueCounts"
+
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod "GET"
+    }
+}
+
 Export-ModuleMember -Function * -Variable *
