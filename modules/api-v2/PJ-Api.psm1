453f3319ba5f2129769ad891a7640d63c75be157
ender2021 <meadmiracle@gmail.com>
2019-03-23 13:42:10 -0700

    Added Format-HashtableToQueryString, updated Invoke-JiraRestRequest to serialize DELETE body correctly

--- modules/api-v2/PJ-Api.psm1
+++ modules/api-v2/PJ-Api.psm1
@@ -64,12 +64,16 @@ function Invoke-JiraRestRequest {
         $function = If($FunctionPath.StartsWith("/")) {$FunctionPath.Substring(1)} else {$FunctionPath}
         $uri = "$hostname/$function"
 
-        if (($null -ne $Body) -and ($Body.Count -gt 0)) {
-            $b = if(($HttpMethod -eq "GET") -or ($HttpMethod -eq "DELETE")) {$Body} else {ConvertTo-Json $Body -Compress -Depth $BodyDepth}
+        if (($null -ne $Body) -and ($Body.Count -gt 0) -and $HttpMethod -ne "DELETE") {
+            $b = if(($HttpMethod -eq "GET")) {$Body} else {ConvertTo-Json $Body -Compress -Depth $BodyDepth}
             Invoke-RestMethod -Uri $uri -Method $HttpMethod -ContentType 'application/json' -Headers $sendHeaders -Body $b
         } elseif ($PSBoundParameters.ContainsKey("LiteralBody")) {
             Invoke-RestMethod -Uri $uri -Method $HttpMethod -ContentType 'application/json' -Headers $sendHeaders -Body $LiteralBody            
         } else {
+            if ($HttpMethod -eq "DELETE" -and ($null -ne $Body) -and ($Body.Count -gt 0)) {
+                $qs = Format-HashtableToQueryString $Body
+                $uri += '?' + $qs
+            }
             Invoke-RestMethod -Uri $uri -Method $HttpMethod -ContentType 'application/json' -Headers $sendHeaders
         }
     }
@@ -137,5 +141,75 @@ function Close-JiraSession() {
         Remove-Variable PJ_JiraSession -Scope Global
     }
 }
+
+#Adapted from: https://www.powershellgallery.com/packages/PSApigeeEdge/0.2.4/Content/Private%5CConvertFrom-HashtableToQueryString.ps1
+function Format-HashtableToQueryString {
+    <#
+    .SYNOPSIS
+      Convert a hashtable into a query string
+
+    .DESCRIPTION
+      Converts a hashtable into a query string by joining the keys to the values,
+      and then joining all the pairs together
+
+    .PARAMETER values
+      The hashtable to convert
+
+    .PARAMETER PairSeparator
+      The string used to concatenate the sets of key=value pairs, defaults to "&"
+
+    .PARAMETER KeyValueSeparator
+      The string used to concatenate the keys to the values, defaults to "="
+
+    .RETURNVALUE
+      The query string created by joining keys to values and then joining
+      them all together into a single string
+
+    .EXAMPLE
+           Format-HashtableToQueryString -Values @{
+                name = 'abcdefg-1'
+                apiProduct = 'Product1'
+                keyExpiresIn = 86400000
+            }
+    #>
+
+param(
+    # The values to format
+    [Parameter(Mandatory)]
+    [hashtable]
+    $Values,
+
+    # The character to use to separate values (default is '&')
+    [Parameter()]
+    [string]
+    $PairSeparator = '&',
+
+    # The value to use to separate key/value pairs
+    [Parameter()]
+    [string]
+    $KeyValueSeparator = '=',
+
+    # Use to sort the outcome
+    [Parameter()]
+    [string[]]
+    $Sort
+)
+process {
+   [string]::join($PairSeparator, @(
+      if($Sort) {
+         foreach( $kv in $Values.GetEnumerator() | Sort $Sort) {
+            if($kv.Name) {
+               '{0}{1}{2}' -f $kv.Name, $KeyValueSeparator, $kv.Value
+            }
+         }
+      } else {
+         foreach( $kv in $Values.GetEnumerator()) {
+            if($kv.Name) {
+               '{0}{1}{2}' -f $kv.Name, $KeyValueSeparator, $kv.Value
+            }
+         }
+      }
+   ))
+}}
 
 Export-ModuleMember -Function * -Variable *
