d0ab322d18f969d374d7e56ef5c3a99a45a14096
ender2021 <meadmiracle@gmail.com>
2019-03-24 22:36:11 -0700

    Added PJ-Group module/demo, added Invoke-JiraFindGroups

---
+++ demos/Demo-Group.ps1
@@ -0,0 +1,18 @@
+#import PowerJira
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath \..\PowerJira.psm1) -Force
+
+#import the variable $JiraCredentials
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath \credentials\Credentials.psm1) -Force
+
+#open a new Jira session
+Open-JiraSession -UserName $JiraCredentials.UserName -Password $JiraCredentials.ApiToken -HostName $JiraCredentials.HostName
+
+#do tests here
+
+#FIND GROUPS
+#Invoke-JiraFindGroups sist @("graddiv-sist-oversight-committee") 1
+
+#end tests
+
+#close the Jira session
+Close-JiraSession
---
+++ modules/api-v2/PJ-Group.psm1
@@ -0,0 +1,36 @@
+#https://developer.atlassian.com/cloud/jira/platform/rest/v2/#api-rest-api-2-groups-picker-get
+function Invoke-JiraFindGroups {
+    [CmdletBinding()]
+    param (
+        # The search string to compare to group names
+        [Parameter(Mandatory,Position=0)]
+        [string]
+        $SearchTerm,
+
+        # A list of groups to exclude from th results
+        [Parameter(Position=1)]
+        [string[]]
+        $Exclude,
+
+        # The maximum number of results to return (also governed by an internal jira setting)
+        [Parameter(Position=2)]
+        [int32]
+        $MaxResults,
+
+        # The JiraConnection object to use for the request
+        [Parameter(Position=3)]
+        [hashtable]
+        $JiraConnection
+    )
+    process {
+        $functionPath = "/rest/api/2/groups/picker"
+
+        $body="query=$SearchTerm"
+        if($Exclude.Count -gt 0) { $Exclude | ForEach-Object {$body+="&exclude=$_"}}
+        if($PSBoundParameters.ContainsKey("MaxResults")){$body+="&maxResults=$MaxResults"}
+
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod "GET" -LiteralBody $body
+    }
+}
+
+Export-ModuleMember -Function * -Variable *
