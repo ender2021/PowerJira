7b7adea3152c5c2afb0645d2f9c38766a5c57b8b
ender2021 <meadmiracle@gmail.com>
2019-03-29 10:30:12 -0700

    Added Invoke-JiraGetPriority

---
+++ PowerJira/public/api-v2/priority/Invoke-JiraGetPriority.ps1
@@ -0,0 +1,20 @@
+#https://developer.atlassian.com/cloud/jira/platform/rest/v2/#api-rest-api-2-priority-id-get
+function Invoke-JiraGetPriority {
+    [CmdletBinding()]
+    param (
+        # The ID of the priority to return
+        [Parameter(Mandatory,Position=0)]
+        [int32]
+        $PriorityId,
+
+        # The JiraConnection object to use for the request
+        [Parameter(Position=1)]
+        [hashtable]
+        $JiraConnection
+    )
+    process {
+        $functionPath = "/rest/api/2/priority/$PriorityId"
+
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod "GET"
+    }
+}
