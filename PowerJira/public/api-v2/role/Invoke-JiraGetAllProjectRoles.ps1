a07e5478ed48f05ea03ed2b4d69a961a216d8025
ender2021 <meadmiracle@gmail.com>
2019-03-30 16:13:04 -0700

    Added Invoke-JiraGetAllProjectRoles

---
+++ PowerJira/public/api-v2/role/Invoke-JiraGetAllProjectRoles.ps1
@@ -0,0 +1,15 @@
+#https://developer.atlassian.com/cloud/jira/platform/rest/v2/#api-rest-api-2-role-get
+function Invoke-JiraGetAllProjectRoles {
+    [CmdletBinding()]
+    param (
+        # The JiraConnection object to use for the request
+        [Parameter(Position=0)]
+        [hashtable]
+        $JiraConnection
+    )
+    process {
+        $functionPath = "/rest/api/2/role"
+
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod "GET"
+    }
+}
