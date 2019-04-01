fca361461f362413ec074620b1f4c57b78fab6e6
ender2021 <meadmiracle@gmail.com>
2019-03-31 17:03:32 -0700

    Added Invoke-JiraGetIssueLinkTypes

---
+++ PowerJira/public/api-v2/issue-link/Invoke-JiraGetIssueLinkType.ps1
@@ -0,0 +1,20 @@
+#https://developer.atlassian.com/cloud/jira/platform/rest/v2/#api-rest-api-2-issueLinkType-issueLinkTypeId-get
+function Invoke-JiraGetIssueLinkType {
+    [CmdletBinding()]
+    param (
+        # THe ID of the issue link type
+        [Parameter(Mandatory,Position=0)]
+        [string]
+        $LinkTypeId,
+
+        # The JiraConnection object to use for the request
+        [Parameter(Position=1)]
+        [hashtable]
+        $JiraConnection
+    )
+    process {
+        $functionPath = "/rest/api/2/issueLinkType/$LinkTypeId"
+
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod "GET"
+    }
+}
