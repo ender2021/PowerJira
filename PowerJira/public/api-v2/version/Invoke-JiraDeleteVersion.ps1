44646c4e30e543f87a7444f118acd7006db291fa
ender2021 <meadmiracle@gmail.com>
2019-04-05 19:50:09 -0700

    Refactored Invoke-JiraDeleteVersion

--- PowerJira/public/api-v2/version/Invoke-JiraDeleteVersion.ps1
+++ PowerJira/public/api-v2/version/Invoke-JiraDeleteVersion.ps1
@@ -3,32 +3,33 @@ function Invoke-JiraDeleteVersion {
     [CmdletBinding()]
     param (
         # The ID of the version to update
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory,Position=0)]
         [int32]
         $VersionId,
 
         # Replacement version for issues with this version in the fixVersions field
-        [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory,Position=1)]
         [int32]
         $FixTargetVersionId,
 
         # Replacement version for issues with this version in the affectsVersions field
-        [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory,Position=2)]
         [int32]
         $AffectedTargetVersionId,
 
         # The JiraConnection object to use for the request
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=3)]
         [hashtable]
         $JiraConnection
     )
     process {
         $functionPath = "/rest/api/2/version/$VersionId/removeAndSwap"
+        $verb = "POST"
         
         $body = @{}
         if($PSBoundParameters.ContainsKey("FixTargetVersionId")){$body.Add("moveFixIssuesTo",$FixTargetVersionId)}
         if($PSBoundParameters.ContainsKey("AffectedTargetVersionId")){$body.Add("moveAffectedIssuesTo",$AffectedTargetVersionId)}
         
-        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod "POST" -Body $body
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod $verb -Body $body
     }
 }
