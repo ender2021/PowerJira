29046f7ec80a2eb91f9ca0f5591b7b6416d8ad7d
ender2021 <meadmiracle@gmail.com>
2019-04-05 20:07:28 -0700

    Refactored Invoke-JiraCreateIssue

--- PowerJira/public/api-v2/issue/Invoke-JiraCreateIssue.ps1
+++ PowerJira/public/api-v2/issue/Invoke-JiraCreateIssue.ps1
@@ -2,51 +2,54 @@
 function Invoke-JiraCreateIssue {
     [CmdletBinding()]
     param (
-        # Indicates whether the creating user's history should be updated to show the project the issue is created in
-        [Parameter(Mandatory=$false)]
-        [Switch]
-        $UpdateUserHistory,
-
-        # DDisables issue notifications for this update
-        [Parameter(Mandatory=$false)]
-        [Switch]
-        $DisableNotifications,
-
-        # ID of a Transition to apply
-        [Parameter(Mandatory=$false)]
-        [int32]
-        $TransitionId,
-
         # Used to make simple updates to fields on this issue
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=0)]
         [hashtable]
         $Fields,
 
         # Used to make complex updates to issue fields
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=1)]
         [hashtable]
         $Update,
 
+        # ID of a Transition to apply
+        [Parameter(Position=2)]
+        [int32]
+        $TransitionId,
+
         # Optional history metadata
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=3)]
         [hashtable]
         $HistoryMetadata,
 
         # Add/set arbitrary issue properties
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=4)]
         [hashtable]
         $Properties,
+        
+        # Indicates whether the creating user's history should be updated to show the project the issue is created in
+        [Parameter(Position=5)]
+        [Switch]
+        $UpdateUserHistory,
+
+        # DDisables issue notifications for this update
+        [Parameter(Position=6)]
+        [Switch]
+        $DisableNotifications,
 
         # The JiraConnection object to use for the request
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=7)]
         [hashtable]
         $JiraConnection
     )
     process {
         $functionPath = "/rest/api/2/issue"
+        $verb = "POST"
+
+        $query = @{}
+        if($PSBoundParameters.ContainsKey("UpdateUserHistory")){$body.Add("updateHistory",$true)}
 
         $body=@{}
-        if($PSBoundParameters.ContainsKey("UpdateUserHistory")){$body.Add("updateHistory",$true)}
         if($PSBoundParameters.ContainsKey("DisableNotifications")){$body.Add("notifyUsers",$false)}
         if($PSBoundParameters.ContainsKey("TransitionId")){$body.Add("transition",@{id="$TransitionId"})}
         if($PSBoundParameters.ContainsKey("Fields")){$body.Add("fields",$Fields)}
@@ -54,6 +57,6 @@ function Invoke-JiraCreateIssue {
         if($PSBoundParameters.ContainsKey("HistoryMetadata")){$body.Add("historyMetadata",$HistoryMetadata)}
         if($PSBoundParameters.ContainsKey("Properties")){$body.Add("properties",$Properties)}
 
-        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod "POST" -Body $body
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod $verb -QueryParams $query -Body $body
     }
  }
