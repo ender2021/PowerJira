698545950d9b82481dcd45e6f822e3c442741443
ender2021 <meadmiracle@gmail.com>
2019-04-05 19:45:22 -0700

    Refactored Invoke-JiraMergeVersion

--- PowerJira/public/api-v2/version/Invoke-JiraMergeVersion.ps1
+++ PowerJira/public/api-v2/version/Invoke-JiraMergeVersion.ps1
@@ -3,23 +3,24 @@ function Invoke-JiraMergeVersion {
     [CmdletBinding()]
     param (
         # The ID of the version to merge from
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory,Position=0)]
         [int32]
         $SourceVersionId,
 
         # The ID of the version to merge into
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory,Position=1)]
         [int32]
         $TargetVersionId,
 
         # The JiraConnection object to use for the request
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=2)]
         [hashtable]
         $JiraConnection
     )
     process {
         $functionPath = "/rest/api/2/version/$SourceVersionId/mergeto/$TargetVersionId"
+        $verb = "PUT"
     
-        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod "PUT"
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod $verb
     }
 }
