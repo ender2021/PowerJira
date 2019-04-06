50afed88e098289d9b52f74188ba9e41f6ff9565
ender2021 <meadmiracle@gmail.com>
2019-04-05 19:41:23 -0700

    Refactored Invoke-JiraGetIssueCreateMetadata

--- PowerJira/public/api-v2/issue/Invoke-JiraGetIssueCreateMetadata.ps1
+++ PowerJira/public/api-v2/issue/Invoke-JiraGetIssueCreateMetadata.ps1
@@ -1,47 +1,51 @@
+$JiraIssueCreateMetaExpand = @("projects.issuetypes.fields")
+
 #https://developer.atlassian.com/cloud/jira/platform/rest/v2/#api-rest-api-2-issue-createmeta-get
 function Invoke-JiraGetIssueCreateMetadata {
     [CmdletBinding()]
     param (
         # IDs of projects to return metadata for
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=0)]
         [int32[]]
         $ProjectIds,
 
         # Keys of projcts to return metadata for
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=1)]
         [string[]]
         $ProjectKeys,
 
         # IDs of issue types to return metadata for
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=2)]
         [int32[]]
         $IssueTypeIds,
 
         # Names of issue types to return metadata for
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=3)]
         [string[]]
         $IssueTypeNames,
 
-        # Expand additional properties
-        [Parameter(Mandatory=$false)]
-        [Switch]
-        $ExpandFields,
+        # Used to expand additional attributes
+        [Parameter(Position=4)]
+        [ValidateScript({ Compare-StringArraySubset $JiraIssueCreateMetaExpand $_ })]
+        [string[]]
+        $Expand,
 
         # The JiraConnection object to use for the request
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=5)]
         [hashtable]
         $JiraConnection
     )
     process {
         $functionPath = "/rest/api/2/issue/createmeta"
+        $verb = "GET"
         
-        $body = @{}
-        if($PSBoundParameters.ContainsKey("ProjectIds")){$body.Add("projectIds",$ProjectIds -join ",")}
-        if($PSBoundParameters.ContainsKey("ProjectKeys")){$body.Add("projectKeys",$ProjectKeys -join ",")}
-        if($PSBoundParameters.ContainsKey("IssueTypeIds")){$body.Add("issuetypeIds",$IssueTypeIds -join ",")}
-        if($PSBoundParameters.ContainsKey("IssueTypeNames")){$body.Add("issuetypeNames",$IssueTypeNames  -join ",")}
-        if($PSBoundParameters.ContainsKey("ExpandFields")){$body.Add("expand","projects.issuetypes.fields  ")}
+        $query = @{}
+        if($PSBoundParameters.ContainsKey("ProjectIds")){$query.Add("projectIds",$ProjectIds -join ",")}
+        if($PSBoundParameters.ContainsKey("ProjectKeys")){$query.Add("projectKeys",$ProjectKeys -join ",")}
+        if($PSBoundParameters.ContainsKey("IssueTypeIds")){$query.Add("issuetypeIds",$IssueTypeIds -join ",")}
+        if($PSBoundParameters.ContainsKey("IssueTypeNames")){$query.Add("issuetypeNames",$IssueTypeNames  -join ",")}
+        if($PSBoundParameters.ContainsKey("Expand")){$query.Add("expand",$Expand -join ",")}
 
-        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod "GET" -Body $body
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod $verb -QueryParams $query
     }
 }
