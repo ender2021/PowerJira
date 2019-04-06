20f1af135bdb7e9cc7a483cd61014ee284ac60a2
ender2021 <meadmiracle@gmail.com>
2019-04-06 15:48:32 -0700

    Refactored Invoke-JiraAddComment

--- PowerJira/public/api-v2/comment/Invoke-JiraAddComment.ps1
+++ PowerJira/public/api-v2/comment/Invoke-JiraAddComment.ps1
@@ -1,3 +1,5 @@
+$JiraCommentExpand = @("renderedBody")
+
 #https://developer.atlassian.com/cloud/jira/platform/rest/v2/#api-rest-api-2-issue-issueIdOrKey-comment-post
 function Invoke-JiraAddComment {
     [CmdletBinding(DefaultParameterSetName="Role")]
@@ -12,51 +14,46 @@ function Invoke-JiraAddComment {
         [string]
         $CommentBody,
 
-        # Set this flag to expand the comment body in the response
+        # Used to expand additional attributes
         [Parameter(Position=2)]
-        [switch]
-        $ExpandRenderedBody,
-
-        # Set this value to restrict the comment visibility to a project role
-        [Parameter(ParameterSetName="Role",Position=3)]
-        [string]
-        $SetRoleVisibility,
-
-        # Set this value to restrict the comment visibility to a group
-        [Parameter(ParameterSetName="Group",Position=4)]
-        [string]
-        $SetGroupVisibility,
+        [ValidateScript({ Compare-StringArraySubset $JiraCommentExpand $_ })]
+        [string[]]
+        $Expand,
+        
+        # Set the visibility of the comment.  Use New-JiraCommentVisibility
+        [Parameter(Position=3)]
+        [hashtable]
+        $Visibility,
 
         # Set this flag to hide this comment in Jira Service Desk
-        [Parameter(Position=5)]
+        [Parameter(Position=4)]
         [switch]
         $JsdHide,
 
         # Additional properties to add to the comment object
-        [Parameter(Position=6)]
+        [Parameter(Position=5)]
         [hashtable[]]
         $Properties,
 
         # The JiraConnection object to use for the request
-        [Parameter(Position=7)]
+        [Parameter(Position=6)]
         [hashtable]
         $JiraConnection
     )
     process {
         $functionPath = "/rest/api/2/issue/$IssueIdOrKey/comment"
+        $verb = "POST"
 
-        #remove this line if Atlassian ever fixes this
-        if($PSBoundParameters.ContainsKey("SetGroupVisibility")){throw "Restricting comment visibility by group is currently broken in the Jira API"}
+        $query = @{}
+        if($PSBoundParameters.ContainsKey("Expand")){$query.Add("expand",$Expand -join ",")}
 
         $body=@{
             body = $CommentBody
         }
-        if($PSBoundParameters.ContainsKey("ExpandRenderedBody")){$body.Add("expand","renderedBody")}
-        if($PSBoundParameters.ContainsKey("SetRoleVisibility")){$body.Add("visibility",@{type="role";value="$SetRoleVisibility"})}
-        if($PSBoundParameters.ContainsKey("SetGroupVisibility")){$body.Add("visibility",@{type="group";value="$SetGroupVisibility"})}
+        if($PSBoundParameters.ContainsKey("Visibility")){$body.Add("visibility",$Visibility)}
         if($PSBoundParameters.ContainsKey("JsdHide")){$body.Add("jsdPublic",$false)}
         if($PSBoundParameters.ContainsKey("Properties")){$body.Add("properties",$Properties)}
 
-        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod "POST" -Body $body
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod $verb -QueryParams $query -Body $body
     }
 }
