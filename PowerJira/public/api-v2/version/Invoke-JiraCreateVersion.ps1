8f92e369784036f4f10002ed98912289b017fd3a
ender2021 <meadmiracle@gmail.com>
2019-04-05 19:53:33 -0700

    Refactored Invoke-JiraCreateVersion

--- PowerJira/public/api-v2/version/Invoke-JiraCreateVersion.ps1
+++ PowerJira/public/api-v2/version/Invoke-JiraCreateVersion.ps1
@@ -1,45 +1,49 @@
+$JiraVersionExpand = @("operations","issuestatus")
+
 # https://developer.atlassian.com/cloud/jira/platform/rest/v2/#api-rest-api-2-version-post
 function Invoke-JiraCreateVersion {
     [CmdletBinding()]
     param (
         # The ID of the project where the version will be created
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory,Position=0)]
         [int32]
         $ProjectId,
 
         # The name of the version to create.  Must be unique within the project.
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory,Position=1)]
         [ValidateLength(1,255)]
         [string]
         $Name,
 
         # A short description of the version
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=2)]
         [string]
         $Description,
 
         # The start date of the version. Will be converted to ISO 8601 format (yyyy-mm-dd). 
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=3)]
         [datetime]
         $StartDate,
 
         # The release date of the version. Will be converted to ISO 8601 format (yyyy-mm-dd). 
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=4)]
         [datetime]
         $ReleaseDate,
 
-        # Returns all possible operations for the issue.
-        [Parameter(Mandatory=$false)]
-        [Switch]
-        $ExpandOperations,
+        # Used to expand additional attributes
+        [Parameter(Position=5)]
+        [ValidateScript({ Compare-StringArraySubset $JiraVersionExpand $_ })]
+        [string[]]
+        $Expand,
         
         # The JiraConnection object to use for the request
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=6)]
         [hashtable]
         $JiraConnection
     )
     process {
         $functionPath = "/rest/api/2/version"
+        $verb = "POST"
     
         $body = @{
             projectId = $ProjectId
@@ -48,8 +52,8 @@ function Invoke-JiraCreateVersion {
         if($PSBoundParameters.ContainsKey("Description")){$body.Add("description",$Description)}
         if($PSBoundParameters.ContainsKey("StartDate")){$body.Add("startDate",(Format-JiraRestDateTime $StartDate))}
         if($PSBoundParameters.ContainsKey("ReleaseDate")){$body.Add("releaseDate",(Format-JiraRestDateTime $ReleaseDate))}
-        if($PSBoundParameters.ContainsKey("ExpandOperations")){$body.Add("expand","operations")}
+        if($PSBoundParameters.ContainsKey("Expand")){$body.Add("expand",$Expand -join ",")}
 
-        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod "POST" -Body $body
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod $verb -Body $body
     }
 }
