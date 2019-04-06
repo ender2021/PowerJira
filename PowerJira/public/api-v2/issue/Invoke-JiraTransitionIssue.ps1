47643415d77de7985260e552f16fce07802d51bd
ender2021 <meadmiracle@gmail.com>
2019-04-05 20:01:34 -0700

    Refactored Invoke-JiraTransitionIssue

--- PowerJira/public/api-v2/issue/Invoke-JiraTransitionIssue.ps1
+++ PowerJira/public/api-v2/issue/Invoke-JiraTransitionIssue.ps1
@@ -3,47 +3,48 @@ function Invoke-JiraTransitionIssue {
     [CmdletBinding()]
     param (
         # The ID or Key of the issue to edit
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory,Position=0)]
         [string]
         $IssueIdOrKey,
 
         # ID of a Transition to apply
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory,Position=1)]
         [int32]
         $TransitionId,
 
         # DDisables issue notifications for this update
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=2)]
         [Switch]
         $DisableNotifications,
 
         # Used to make simple updates to fields on this issue
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=3)]
         [hashtable]
         $Fields,
 
         # Used to make complex updates to issue fields
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=4)]
         [hashtable]
         $Update,
 
         # Optional history metadata
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=5)]
         [hashtable]
         $HistoryMetadata,
 
         # Add/set arbitrary issue properties
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=6)]
         [hashtable]
         $Properties,
 
         # The JiraConnection object to use for the request
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=7)]
         [hashtable]
         $JiraConnection
     )
     process {
         $functionPath = "/rest/api/2/issue/$IssueIdOrKey/transitions"
+        $verb = "POST"
 
         $body=@{
             transition=@{id="$TransitionId"}
@@ -54,6 +55,6 @@ function Invoke-JiraTransitionIssue {
         if($PSBoundParameters.ContainsKey("HistoryMetadata")){$body.Add("historyMetadata",$HistoryMetadata)}
         if($PSBoundParameters.ContainsKey("Properties")){$body.Add("properties",$Properties)}
 
-        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod "POST" -Body $body
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod $verb -Body $body
     }
 }
