c9ef3de83eae31f1f732ec7d7c4b099f797b1f6f
ender2021 <meadmiracle@gmail.com>
2019-04-05 19:43:31 -0700

    Refactored Invoke-JiraMoveVersion

--- PowerJira/public/api-v2/version/Invoke-JiraMoveVersion.ps1
+++ PowerJira/public/api-v2/version/Invoke-JiraMoveVersion.ps1
@@ -3,32 +3,33 @@ function Invoke-JiraMoveVersion {
     [CmdletBinding()]
     param (
         # The ID of the version to update
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory,Position=0)]
         [int32]
         $VersionId,
 
         # Move this version to immediately after another specified target version. Provide the self link of the target.
-        [Parameter(Mandatory=$true,ParameterSetName="After")]
+        [Parameter(Mandatory,Position=1,ParameterSetName="After")]
         [uri]
         $After,
 
         # Move this version to a new position relative to the list or itself.
-        [Parameter(Mandatory=$true,ParameterSetName="Position")]
+        [Parameter(Mandatory,Position=1,ParameterSetName="Position")]
         [ValidateSet("Earlier","Later","First","Last")]
         [string]
         $Position,
         
         # The JiraConnection object to use for the request
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=2)]
         [hashtable]
         $JiraConnection
     )
     process {
         $functionPath = "/rest/api/2/version/$VersionId/move"
+        $verb = "POST"
         
         $body = @{}
         if($PSBoundParameters.ContainsKey("After")){$body.Add("after",$After)} else {$body.Add("position",$Position)}
 
-        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod "POST" -Body $body
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod $verb -Body $body
     }
 }
