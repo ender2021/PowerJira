6620613d2434193955b126b45c512b4f6a89da98
ender2021 <meadmiracle@gmail.com>
2019-04-05 20:11:10 -0700

    Refactored Invoke-JiraEditIssue

--- PowerJira/public/api-v2/issue/Invoke-JiraEditIssue.ps1
+++ PowerJira/public/api-v2/issue/Invoke-JiraEditIssue.ps1
@@ -3,50 +3,53 @@ function Invoke-JiraEditIssue {
     [CmdletBinding()]
     param (
         # The ID or Key of the issue to edit
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory,Position=0)]
         [string]
         $IssueIdOrKey,
 
-        # DDisables issue notifications for this update
-        [Parameter(Mandatory=$false)]
-        [Switch]
-        $DisableNotifications,
-
         # Used to make simple updates to fields on this issue
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=1)]
         [hashtable]
         $Fields,
 
         # Used to make complex updates to issue fields
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=2)]
         [hashtable]
         $Update,
 
         # Optional history metadata
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=3)]
         [hashtable]
         $HistoryMetadata,
 
         # Add/set arbitrary issue properties
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=4)]
         [hashtable]
         $Properties,
 
+        # DDisables issue notifications for this update
+        [Parameter(Position=5)]
+        [Switch]
+        $DisableNotifications,
+
         # The JiraConnection object to use for the request
-        [Parameter(Mandatory=$false)]
+        [Parameter(Position=6)]
         [hashtable]
         $JiraConnection
     )
     process {
         $functionPath = "/rest/api/2/issue/$IssueIdOrKey"
+        $verb = "PUT"
+
+        $query = @{}
+        if($PSBoundParameters.ContainsKey("DisableNotifications")){$body.Add("notifyUsers",$false  )}
 
         $body=@{}
-        if($PSBoundParameters.ContainsKey("DisableNotifications")){$body.Add("notifyUsers",$false)}
         if($PSBoundParameters.ContainsKey("Fields")){$body.Add("fields",$Fields)}
         if($PSBoundParameters.ContainsKey("Update")){$body.Add("update",$Update)}
         if($PSBoundParameters.ContainsKey("HistoryMetadata")){$body.Add("historyMetadata",$HistoryMetadata)}
         if($PSBoundParameters.ContainsKey("Properties")){$body.Add("properties",$Properties)}
 
-        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod "PUT" -Body $body
+        Invoke-JiraRestRequest -JiraConnection $JiraConnection -FunctionPath $functionPath -HttpMethod $verb -QueryParams $query -Body $body
     }
 }
