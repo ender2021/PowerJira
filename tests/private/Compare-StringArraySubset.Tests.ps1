e9bf59d76e6885e5d156e4bae2458e23dc188480
ender2021 <meadmiracle@gmail.com>
2019-04-06 23:42:38 -0700

    Added tests for Compare-StringArraySubset

---
+++ tests/private/Compare-StringArraySubset.Tests.ps1
@@ -0,0 +1,31 @@
+Describe "Compare-StringArraySubset" {
+    Context "Should return true" {
+        It "lists match exactly" {
+            $super = @("one","two","three","four")
+            $sub = $super
+            Compare-StringArraySubset $super $sub | Should -Be $true
+        }
+        It "sub has one item from super" {
+            $super = @("one","two","three","four")
+            $sub = @("one")
+            Compare-StringArraySubset $super $sub | Should -Be $true
+        }
+        It "sub has multiple items from super" {
+            $super = @("one","two","three","four")
+            $sub = @("one","three","four")
+            Compare-StringArraySubset $super $sub | Should -Be $true
+        }
+    }
+    Context "Should return false" {
+        It "sub has no items from super" {
+            $super = @("one","two","three","four")
+            $sub = @("red","green","blue")
+            Compare-StringArraySubset $super $sub | Should -Be $false
+        }
+        It "sub has some items from super, and some not in super" {
+            $super = @("one","two","three","four")
+            $sub = @("one","two","blue","green")
+            Compare-StringArraySubset $super $sub | Should -Be $false
+        }
+    }
+}
