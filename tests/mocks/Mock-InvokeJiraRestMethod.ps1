a328e09a97b6d1f7dbf5bf5211e99cbe7435b976
ender2021 <meadmiracle@gmail.com>
2019-04-07 20:49:13 -0700

    Added Mock-InvokeJiraRestMethod

---
+++ tests/mocks/Mock-InvokeJiraRestMethod.ps1
@@ -0,0 +1,13 @@
+$MockInvokeJiraRestMethod = {
+    @{
+        JiraConnection = $JiraConnection
+        FunctionPath = $FunctionPath
+        HttpMethod = $HttpMethod
+        Headers = $Headers
+        Query = $Query
+        QueryKvp = $QueryKvp
+        Body = $Body
+        LiteralBody = $LiteralBody
+        Form = $Form
+    }
+}
