df0f0d0be29ad62911cb9f6b75f290022c3bb0ff
ender2021 <meadmiracle@gmail.com>
2019-09-19 13:56:19 -0700

    Added Invoke-JiraGetAllLabels

---
+++ demos/Demo-Label.ps1
@@ -0,0 +1,18 @@
+#import PowerJira
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath \..\PowerJira\PowerJira.psm1) -Force
+
+#import the variable $JiraCredentials
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath \credentials\Credentials.psm1) -Force
+
+#open a new Jira session
+Open-JiraSession -UserName $JiraCredentials.UserName -Password $JiraCredentials.ApiToken -HostName $JiraCredentials.HostName
+
+#do tests here
+
+#GET ALL LABELS
+#Invoke-JiraGetAllLabels
+
+#end tests
+
+#close the Jira session
+Close-JiraSession
