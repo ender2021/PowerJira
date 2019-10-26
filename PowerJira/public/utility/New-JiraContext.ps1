c5d37da03f43bb8e9553d98e8aec2075b27ea38e
ender2021 <meadmiracle@gmail.com>
2019-10-26 11:27:11 -0700

    created a function to instantiate a new Jira Context

---
+++ PowerJira/public/utility/New-JiraContext.ps1
@@ -0,0 +1,24 @@
+function New-JiraContext {
+    [CmdletBinding(DefaultParameterSetName="PlainText")]
+    param (
+        # The Jira username of the user performing actions
+        [Parameter(Mandatory,Position=0,ParameterSetName="PlainText")]
+        [string]
+        $UserName,
+
+        # The Jira password (or API Token) of the user performing actions
+        [Parameter(Mandatory,Position=1,ParameterSetName="PlainText")]
+        [string]
+        $Password,
+
+        # The hostname of the Jira instance to interact with (e.g. https://yourjirasite.atlassian.net/)
+        [Parameter(Mandatory,Position=2)]
+        [string]
+        $HostName
+    )
+    begin {}
+    process {
+        return [JiraContext]::new($UserName,$Password,$HostName)
+    }
+    end {}
+}
