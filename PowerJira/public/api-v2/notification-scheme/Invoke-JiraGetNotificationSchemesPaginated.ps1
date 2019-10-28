a76d0b967fced811e714d31e77f735c5af66152b
ender2021 <meadmiracle@gmail.com>
2019-10-27 17:33:27 -0700

    convert notification scheme functsion to use classes

--- PowerJira/public/api-v2/notification-scheme/Invoke-JiraGetNotificationSchemesPaginated.ps1
+++ PowerJira/public/api-v2/notification-scheme/Invoke-JiraGetNotificationSchemesPaginated.ps1
@@ -21,21 +21,22 @@ function Invoke-JiraGetNotificationSchemesPaginated {
         [string[]]
         $Expand,
 
-        # The JiraConnection object to use for the request
-        [Parameter(Position=3)]
-        [hashtable]
-        $JiraConnection
+        # The JiraContext object to use for the request
+        [Parameter()]
+        [JiraContext]
+        $JiraContext
     )
     process {
         $functionPath = "/rest/api/2/notificationscheme"
         $verb = "GET"
 
-        $query = @{
+        $query = New-Object RestMethodQueryParams @{
             startAt = $StartAt
             maxResults = $MaxResults
         }
         if($PSBoundParameters.ContainsKey("Expand")){$query.Add("expand",$Expand -join ",")}
 
-        Invoke-JiraRestMethod $JiraConnection $functionPath $verb -Query $query
+        $method = New-Object RestMethod @($functionPath,$verb,$query)
+        $method.Invoke($JiraContext)
     }
 }
