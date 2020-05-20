6d4dfbd07f93e78b6d4dd458b1963ebe8f67694b
ReubenUnruh <8311571+ReubenUnruh@users.noreply.github.com>
2020-05-20 13:40:09 -0700

    Add Invoke-JiraGetDeployments (#1)
    * Add Invoke-JiraGetDeployments + demo

---
+++ PowerJira/public/api-v2/issue/Invoke-JiraGetDeployments.ps1
@@ -0,0 +1,59 @@
+#https://developer.atlassian.com/cloud/jira/platform/rest/v2/#api-rest-api-2-issue-issueIdOrKey-get
+function Invoke-JiraGetDeployments {
+    param (
+        # The ID of the issue
+        [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [int32]
+        $Id
+    )
+    begin {
+        $results = @()
+    }
+    process {
+        $functionPath = "/jsw/graphql"
+        $verb = "POST"
+
+        $query = New-Object RestMethodQueryParams @{
+            operation = "DevDetailsDialog"
+        }
+
+        $gqlQuery = @'
+query DevDetailsDialog ($issueId: ID!) {
+	developmentInformation(issueId: $issueId){
+		details {
+			deploymentProviders {
+				deployments {
+					displayName
+					url
+					state
+					lastUpdated
+					pipelineId
+					pipelineDisplayName
+					pipelineUrl
+					environment {
+						id
+						type
+						displayName
+					}
+				}
+			}
+		}
+	}
+}
+'@
+
+        $body = New-Object RestMethodJsonBody @{
+            operationName = "DevDetailsDialog"
+            query         = $gqlQuery
+            variables     = @{
+                issueId = $Id
+            }
+        }
+
+        $method = New-Object BodyRestMethod @($functionPath, $verb, $query, $body) 
+        $results += $method.Invoke($JiraContext)
+    }
+    end {
+        $results.data.developmentInformation.details.deploymentProviders.deployments
+    }
+}
