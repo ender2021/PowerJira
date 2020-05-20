5381b3247ab7b890196396bb75e60516ddf5a4e4
ender2021 <meadmiracle@gmail.com>
2020-05-20 16:47:32 -0700

    Version 1.1.3 !deploy

--- build.ps1
+++ build.ps1
@@ -39,6 +39,7 @@ function Resolve-Module
             {
                 Write-Verbose -Message "$($ModuleName) Missing, installing Module"
                 Install-Module -Name $ModuleName -Force
+                "$ModuleName Installed"
                 Import-Module -Name $ModuleName -Force -RequiredVersion $Version
             }
         }
