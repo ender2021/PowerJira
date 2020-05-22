ddc82fb1473e1fc5396b6ae71b7fcd474515120a
ender2021 <meadmiracle@gmail.com>
2020-05-22 13:55:30 -0700

    fixed demo file

--- demos/Demo-ProjectCategory.ps1
+++ demos/Demo-ProjectCategory.ps1
@@ -1,6 +1,5 @@
 #import PowerJira
-#Import-Module (Join-Path -Path $PSScriptRoot -ChildPath \..\PowerJira\PowerJira.psm1) -Force
-Import-Module PowerJira
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath \..\PowerJira\PowerJira.psm1) -Force
 
 #import the variable $JiraCredentials
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath \credentials\Credentials.psm1) -Force
@@ -11,7 +10,7 @@ $context = Open-JiraSession -UserName $JiraCredentials.UserName -Password $JiraC
 #do tests here
 
 #GET ALL PROJECT CATEGORIES
-Invoke-JiraGetAllProjectCategories
+#Invoke-JiraGetAllProjectCategories
 
 #CREATE PROJECT CATEGORY
 #Invoke-JiraCreateProjectCategory "new category" "this is a description"
@@ -28,4 +27,4 @@ Invoke-JiraGetAllProjectCategories
 #end tests
 
 #close the Jira session
-$context.CloseSession()
+Close-JiraSession
