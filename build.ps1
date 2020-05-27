aa906815d17773ad7d23d86ecda985de8bcae88a
ender2021 <meadmiracle@gmail.com>
2020-05-27 15:58:30 -0700

    setting pester version to 4.9.0

--- build.ps1
+++ build.ps1
@@ -65,7 +65,7 @@ function Resolve-Module
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Resolve-Module -Name Psake, PSDeploy, Pester, BuildHelpers, PowerAtlassianCore -RequiredVersions @{Pester="3.4.0"}
+Resolve-Module -Name Psake, PSDeploy, Pester, BuildHelpers, PowerAtlassianCore -RequiredVersions @{Pester="4.9.0"}
 
 Set-BuildEnvironment
 
